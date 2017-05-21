14671a70e4334278854f7e675073fa10c85110b1
huyan <515253685@qq.com>
2017-05-21 19:55:32 +0800

    Add tag create page

--- manager/src/axios/tag.js
+++ manager/src/axios/tag.js
@@ -3,6 +3,9 @@ import instance from './instance'
 var tagResource = {
 	getTags: function(){
 		return instance.get('/tag');
+	},
+	createOneTag: function(data){
+		return instance.post('/tag', data);
 	}
 }
 export default tagResource;
--- manager/src/components/Tag/createTag.vue
+++ manager/src/components/Tag/createTag.vue
@@ -1,29 +1,50 @@
 <template>
 <div>
-	<el-form label-position="top" label-width="80px" :model="formLabelAlign">
-  <el-form-item label="文章标题">
-    <el-input v-model="formLabelAlign.name"></el-input>
+  <el-form label-position="left" label-width="80px" :model="newtag" :inline="true">
+  <el-form-item label="添加标签" required>
+    <el-input v-model="newtag.tag_name" placeholder="标签"></el-input>
   </el-form-item>
-  <el-form-item label="文章分类">
-    <el-input v-model="formLabelAlign.region"></el-input>
-  </el-form-item>
-  <el-form-item label="文章标签">
-    <el-input v-model="formLabelAlign.type"></el-input>
+  <el-form-item>
+    <el-button type="primary" @click="submitTag()">创建</el-button>
   </el-form-item>
 </el-form>
-</div>	
+</div>  
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        labelPosition: 'right',
-        formLabelAlign: {
-          name: '',
-          region: '',
-          type: ''
-        }
-      };
+import tagResource from '../../axios/tag' 
+export default {
+  data() {
+    return {
+      newtag: {
+        tag_name: ''
+      }
+    };
+  },
+  methods:{
+    createOneTag(){
+      var vm = this;
+      return tagResource.createOneTag(vm.newtag).then(function(res) {
+        console.log(res.data);
+      })
+    },
+    submitTag(){
+      this.$confirm('确定创建标签类吗, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.createOneTag(this.newtag);
+          this.$message({
+            type: 'success',
+            message: '创建成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消创建'
+          });          
+        }); 
     }
-  }	
+  }
+} 
 </script>
